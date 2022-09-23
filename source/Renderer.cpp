816a842984010dd65636488a8011fc7f268dd2eb
Twannes <Twannes02@gmail.com>
2022-09-23 15:41:15 +0200

    Checking A persons code if it works, but his framework was just broken

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -27,41 +27,76 @@ void Renderer::Render(Scene* pScene) const
 	auto& materials = pScene->GetMaterials();
 	auto& lights = pScene->GetLights();
 
+	//for (int px{}; px < m_Width; ++px)
+	//{
+	//	for (int py{}; py < m_Height; ++py)
+	//	{
+
+	//		float ar{ float(m_Width) / float(m_Height) };
+
+	//		Vector3 rayDirection{};
+
+	//		rayDirection.x = (((2 * (px + 0.5f)) / m_Width) - 1) * ar;
+
+	//		rayDirection.y = 1 - ((2 * (py + 0.5f)) / m_Height);
+
+	//		rayDirection.z = 1.f;
+
+	//		rayDirection.Normalize();
+
+
+	//		Ray viewRay{ {0,0,0}, rayDirection };
+
+	//		ColorRGB finalColor{};
+
+	//		HitRecord closestHit{};
+
+	//		pScene->GetClosestHit(viewRay, closestHit);
+
+	//		if (closestHit.didHit)
+	//		{
+	//			finalColor = materials[closestHit.materialIndex]->Shade();
+
+	//			/*const float scaled_t = closestHit.t / 500.f;
+	//			finalColor = { scaled_t, scaled_t, scaled_t };*/
+	//		}
+
+	//		//Update Color in Buffer
+	//		finalColor.MaxToOne();
+
+	//		m_pBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBuffer->format,
+	//			static_cast<uint8_t>(finalColor.r * 255),
+	//			static_cast<uint8_t>(finalColor.g * 255),
+	//			static_cast<uint8_t>(finalColor.b * 255));
+	//	}
+	//}
+
 	for (int px{}; px < m_Width; ++px)
 	{
 		for (int py{}; py < m_Height; ++py)
 		{
+			float aspectRatio{ float(m_Width) / float(m_Height) };
 
-			float ar{ float(m_Width) / float(m_Height) };
-
-			Vector3 rayDirection{};
-
-			rayDirection.x = (((2 * (px + 0.5f)) / m_Width) - 1) * ar;
-
-			rayDirection.y = 1 - ((2 * (py + 0.5f)) / m_Height);
-
-			rayDirection.z = 1.f;
-
+			Vector3 rayDirection;
+			float offset{ 0.5f };
+			rayDirection.x = ((2.f * (px + offset) / float(m_Width)) - 1) * aspectRatio;
+			rayDirection.y = (1.f - 2.f * (py + offset) / m_Height);
+			rayDirection.z = 1;
 
 			rayDirection.Normalize();
 
-
-			Ray viewRay{ {0,0,0}, rayDirection };
+			Ray viewRay{ Vector3{0,0,0},rayDirection };
 
 			ColorRGB finalColor{};
 
 			HitRecord closestHit{};
 
-			pScene->GetClosestHit(viewRay, closestHit);
-
+			Sphere testSphere{ Vector3{0.f,0.f,100.f},50.f,0 };
+			GeometryUtils::HitTest_Sphere(testSphere, viewRay, closestHit);
 			if (closestHit.didHit)
 			{
 				finalColor = materials[closestHit.materialIndex]->Shade();
-
-				/*const float scaled_t = closestHit.t / 500.f;
-				finalColor = { scaled_t, scaled_t, scaled_t };*/
 			}
-
 			//Update Color in Buffer
 			finalColor.MaxToOne();
 
